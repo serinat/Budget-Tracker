62fd3b0d82e5b018c5e7e6736fcf86266dad6204
Serina Tsang <serinat@uw.edu>
2020-02-01 20:28:18 -0800

    app working

--- models/transaction.js
+++ models/transaction.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema({
     name: { type: String, trim: true, required: "Enter a name for transaction" },
-    value: { type: Number, required: "Enter an amout" },
+    value: { type: Number, required: "Enter an amount" },
     date: { type: Date, default: Date.now }
 });
 
